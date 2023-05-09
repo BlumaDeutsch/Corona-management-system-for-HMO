ef76384ae6db81e1b0e76ff8f30b60e5d386c450
Bluma Deutsch <80222387+BlumaDeutsch@users.noreply.github.com>
2023-05-09 23:07:12 +0300

    Add files via upload

--- Corona management system for HMO.py
+++ Corona management system for HMO.py
@@ -1,19 +1,20 @@
 import pymongo
-import flask
+from flask import *
+import json
 
 
 class Person:
-    def __init__(self, name, last_name, id_number, city=None, street=None, number=None, date_of_birth=None, phone=None,
+    def __init__(self, name, last_name, id_number, date_of_birth, phone, city=None, street=None, number=None,
                  mobile_phone=None, date1=None, manufacturer1=None, date2=None, manufacturer2=None, date3=None,
                  manufacturer3=None, date4=None, manufacturer4=None, positive_result=None, recovery_date=None):
         self.name = name
         self.last_name = last_name
         self.id_number = id_number
+        self.date_of_birth = date_of_birth
+        self.phone = phone
         self.city = city
         self.street = street
         self.number = number
-        self.date_of_birth = date_of_birth
-        self.phone = phone
         self.mobile_phone = mobile_phone
         self.date1 = date1
         self.manufacturer1 = manufacturer1
@@ -27,40 +28,82 @@ class Person:
         self.recovery_date = recovery_date
 
     def insert_person_to_db(self):
-        collection.insert_one({"name": self.name, "last_name": self.last_name, "id_number": self.id_number})
+        collection.insert_one(
+            Person.convert_data_to_json(self.name, self.last_name, self.id_number, self.date_of_birth, self.phone,
+                                        self.city, self.street, self.number, self.mobile_phone, self.date1,
+                                        self.manufacturer1, self.date2, self.manufacturer2, self.date3,
+                                        self.manufacturer3, self.date4, self.manufacturer4, self.positive_result,
+                                        self.recovery_date))
 
     @classmethod
     def find_person_by_id(cls, id_number):
-        return collection.find({"id_number": id_number}, {"_id": 0, "id_number": 0}).next()
+        return collection.find({"id_number": id_number}, {"_id": 0}).next()
 
-    def corona_data_update(self):
+    @classmethod
+    def convert_data_to_json(cls, name, last_name, id_number, date_of_birth, phone, city, street, number, mobile_phone,
+                             date1, manufacturer1, date2, manufacturer2, date3, manufacturer3, date4, manufacturer4,
+                             positive_result, recovery_date):
+        return dict(name=name, last_name=last_name, id_number=id_number, date_of_birth=date_of_birth, phone=phone,
+                    city=city, street=street, number=number, mobile_phone=mobile_phone, date1=date1,
+                    manufacturer1=manufacturer1, date2=date2, manufacturer2=manufacturer2, date3=date3,
+                    manufacturer3=manufacturer3, date4=date4, manufacturer4=manufacturer4,
+                    positive_result=positive_result,
+                    recovery_date=recovery_date)
+
+    def data_update(self):
         pass
+
+
+def convert_json_to_string(json_string):
+    ordered_str = json.dumps(json_string)
+    ordered_str = ordered_str.replace('"', "")
+    ordered_str = ordered_str.replace(', ', '\n')
+    return ordered_str[1:-1]
 
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["HMOInformation"]
 collection = db["PersonalInformation"]
 
+app = Flask(__name__)
 
-app = flask.Flask(__name__)
+
+@app.route('/')
+def login():
+    return render_template('login.html')
 
 
 @app.route('/add_person')
 def add_person():
-    person = Person(flask.request.args.get('name'), flask.request.args.get('last_name'), flask.request.args.get('id_number'))
+    return render_template('add_person.html')
+
+
+@app.route('/create_person', methods=['POST', 'GET'])
+def create_person():
+    person = Person(request.form['name'], request.form['last_name'], request.form['id_number'],
+                    request.form['date_of_birth'], request.form['phone'], request.form['city'],
+                    request.form['street'], request.form['number'], request.form['mobile_phone'], request.form['date1'],
+                    request.form['man1'], request.form['date2'], request.form['man2'], request.form['date3'],
+                    request.form['man3'], request.form['date4'], request.form['man4'], request.form['positive'],
+                    request.form['recovery'])
     person.insert_person_to_db()
-    return '%s is added to the data base!' % flask.request.args.get('name')
+    return redirect(url_for('login'))
+
+
+@app.route('/search_person')
+def search_person():
+    id_number = request.args.get('id_number')
+    try:
+        person = Person.find_person_by_id(id_number)
+    except StopIteration:
+        return render_template('find_person.html', person="The requested person is not found, Please try again")
+    else:
+        return render_template('find_person.html', person=convert_json_to_string(person))
 
 
 @app.route('/find_person')
 def find_person():
-    id_number = flask.request.args.get('id_number')
-    return Person.find_person_by_id(id_number)
-
-
-@app.route('/update_corona_data')
-def update():
-    return 'done'
+    return render_template('find_person.html')
 
 
 app.run(debug=True)
